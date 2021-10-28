f7a5f68c572f8acb1d2c5b0f207cd9084ca73a84
PandersPanda <anderh0709@gmail.com>
2021-10-28 14:34:02 +0200

    Delt inn i tre forskjellige controllers osv

--- WebApp2/Controllers/ReiseController.cs
+++ WebApp2/Controllers/ReiseController.cs
@@ -1,11 +1,86 @@
-﻿using System;
+﻿using Kunde_SPA.DAL;
+using Kunde_SPA.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Kunde_SPA.Controllers
 {
-    public class ReiseController
+    public class ReiseController : ControllerBase
     {
+        private IReiseRepository _billettDb;
+
+        private ILogger<ReiseController> _log;
+
+        private const string _loggetInn = "loggetInn";
+
+        public ReiseController(IReiseRepository billettDb, ILogger<ReiseController> log)
+        {
+            _billettDb = billettDb;
+            _log = log;
+        }
+        public async Task<ActionResult> HentAlleReiser()
+        {
+            //if (string.IsNullOrEmpty(HttpContext.Session.GetString(_loggetInn)))
+            //{
+            //    return Unauthorized("Ikke logget inn");
+            //}
+            List<Reise> reiseListe = await _billettDb.HentAlleReiser();
+            return Ok(reiseListe);
+        }
+
+        public async Task<ActionResult> HentEnReise(int id)
+        {
+            Reise hentetReise = await _billettDb.HentEnReise(id);
+
+            if (hentetReise == null)
+            {
+                _log.LogInformation("Fant ikke reisen i databasen");
+                return NotFound();
+            }
+            return Ok(hentetReise);
+        }
+
+        public async Task<ActionResult> LagreReise(Reise innReise)
+        {
+            bool lagreOk = await _billettDb.LagreReise(innReise);
+            if (!lagreOk)
+            {
+                _log.LogInformation("Reisen kunne ikke lagres!");
+                return BadRequest();
+            }
+            return Ok();
+        }
+
+        //EndreReise
+        public async Task<ActionResult> EndreReise(Reise endreReise)
+        {
+            if (ModelState.IsValid)
+            {
+                bool endreOk = await _billettDb.EndreReise(endreReise);
+                if (!endreOk)
+                {
+                    _log.LogInformation("Reisen kunne ikke bli endret");
+                    return NotFound();
+                }
+                return Ok();
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest();
+        }
+
+        public async Task<ActionResult> SlettReise(int id)
+        {
+            bool returOK = await _billettDb.SlettReise(id);
+            if (!returOK)
+            {
+                _log.LogInformation("Reisen ble ikke slettet");
+                return NotFound();
+            }
+            return Ok();
+        }
     }
 }
