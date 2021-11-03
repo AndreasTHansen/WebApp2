7600c1162e4b51899ef2c7adbb064c344236c488
AndreasTHansen <andreas-torres@hotmail.com>
2021-11-03 16:23:54 +0100

    Har laged bruker-controller, bruker repo og iBruker repo. Slettet innlogging fra billettcontroller

---
+++ WebApp2/Controllers/BrukerController.cs
@@ -0,0 +1,53 @@
+﻿using Kunde_SPA.DAL;
+using Kunde_SPA.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kunde_SPA.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class BrukerController : ControllerBase
+    {
+
+        private readonly IBillettRepository _billettDb;
+
+        private ILogger<BrukerController> _log;
+
+        private const string _loggetInn = "loggetInn";
+        private const string _ikkeLoggetInn = "";
+
+        public BrukerController(IBillettRepository billettDb, ILogger<BrukerController> log)
+        {
+            _billettDb = billettDb;
+            _log = log;
+        }
+
+        //Hentet fra fagstoff
+        [HttpPost]
+        public async Task<ActionResult> LoggInn(Bruker bruker)
+        {
+            if (ModelState.IsValid)
+            {
+                bool returnOK = await _billettDb.LoggInn(bruker);
+                if (!returnOK)
+                {
+                    _log.LogInformation("Innloggingen feilet for bruker");
+                    HttpContext.Session.SetString(_loggetInn, _ikkeLoggetInn);
+                    return Ok(false);
+                }
+                HttpContext.Session.SetString(_loggetInn, _loggetInn);
+                return Ok(true);
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering på server");
+        }
+
+    }
+}
---
+++ WebApp2/DAL/BrukerRepository.cs
@@ -0,0 +1,71 @@
+﻿using Kunde_SPA.Model;
+using KundeApp2.DAL;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Kunde_SPA.DAL
+{
+    public class BrukerRepository : IBrukerRepository
+    {
+
+        private readonly DBContext _billettDb;
+
+        private ILogger<BillettRepository> _log;
+
+        public BrukerRepository(DBContext billettDb, ILogger<BillettRepository> log)
+        {
+            _billettDb = billettDb;
+            _log = log;
+
+        }
+
+
+        //LagHash, LagSalt og LoggInn er hentet fra fagstoff.
+        public static byte[] LagHash(string passord, byte[] salt)
+        {
+            return KeyDerivation.Pbkdf2(
+                                password: passord,
+                                salt: salt,
+                                prf: KeyDerivationPrf.HMACSHA512,
+                                iterationCount: 1000,
+                                numBytesRequested: 32);
+        }
+
+        public static byte[] LagSalt()
+        {
+            var csp = new RNGCryptoServiceProvider();
+            var salt = new byte[24];
+            csp.GetBytes(salt);
+            return salt;
+        }
+
+
+        public async Task<bool> LoggInn(Bruker bruker)
+        {
+            try
+            {
+                Brukere funnetBruker = await _billettDb.Brukere.FirstOrDefaultAsync(b => b.Brukernavn == bruker.Brukernavn);
+
+                //sjekk passordet
+                byte[] hash = LagHash(bruker.Passord, funnetBruker.Salt);
+                bool ok = hash.SequenceEqual(funnetBruker.Passord);
+                if (ok)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception e)
+            {
+                _log.LogInformation(e.Message);
+                return false;
+            }
+        }
+    }
+}
