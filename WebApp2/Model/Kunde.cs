bdc615b0d87d1ad296b90ef37254cbac4c0e2817
PandersPanda <anderh0709@gmail.com>
2021-11-04 15:44:11 +0100

    commit da

--- WebApp2/Model/Kunde.cs
+++ WebApp2/Model/Kunde.cs
@@ -8,8 +8,11 @@ namespace KundeApp2.Model
         public int id { get; set; }
         [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,30}")]
         public string fornavn { get; set; }
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,50}$")]
         public string etternavn { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")]
         public string epost { get; set; }
+        [RegularExpression(@"^[0-9\.\ \-]{8,12}$")]
         public string mobilnummer { get; set; }
         public string kortnummer { get; set; }
         public string utlopsdato { get; set; }
